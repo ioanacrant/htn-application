fe7f72a590423b0864ba6490fdaece6372e86426
Ioana Crant <ioana.crant@gmail.com>
2016-02-27 16:46:35 -0500

    got user info from json

--- create_db.py
+++ create_db.py
@@ -1,28 +1,40 @@
 import sqlite3
 import json
-
-#JSON_FILE =
-DB_FILE = "users.db"
-
-#traffic = json.load(open(JSON_FILE))
-connection = sqlite3.connect(DB_FILE)
+import requests
 
 
-c = connection.cursor()
-c.execute('''CREATE TABLE users (
-                id INTEGER PRIMARY KEY,
-                name TEXT,
-                picture TEXT,
-                company TEXT,
-                email TEXT,
-                phone TEXT,
-                country TEXT,
-                latitude NUMBER,
-                longitude NUMBER)''')
+def create_db(db="users.db"):
+    connection = sqlite3.connect(DB_FILE)
+    c = connection.cursor()
+    c.execute('''CREATE TABLE users (
+                    id INTEGER PRIMARY KEY,
+                    name TEXT,
+                    picture TEXT,
+                    company TEXT,
+                    email TEXT,
+                    phone TEXT,
+                    country TEXT,
+                    latitude NUMBER,
+                    longitude NUMBER)''')
 
-c.execute('''CREATE TABLE skills (
-                skill_id INTEGER PRIMARY KEY,
-                user_id INTEGER,
-                name TEXT,
-                rating NUMBER,
-                FOREIGN KEY (user_id) REFERENCES users(id))''')
+    c.execute('''CREATE TABLE skills (
+                    skill_id INTEGER PRIMARY KEY,
+                    user_id INTEGER,
+                    name TEXT,
+                    rating NUMBER,
+                    FOREIGN KEY (user_id) REFERENCES users(id))''')
+
+#def populate_users(db="users.db"):
+
+
+
+def isolate_user_info():
+    JSON_FILE = requests.get("https://htn-interviews.firebaseio.com/users.json").json()
+    userarray = []
+    for user in JSON_FILE:
+        print(user)
+        userarray.append(user["name"], user["picture"],user["company"], user["email"], user["phone"],
+                        user["country"], user["latitude"], user["longitude"])
+    return userarray
+
+isolate_user_info()
